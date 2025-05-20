60d15e361f58a9780daa2f11c4d28d0a2c519dec
hunainsh-99 <hunainsheikh5@gmail.com>
2025-05-20 15:12:22 -0400

    perf: add Locust load test for homepage & login flows

---
+++ performance/locustfile.py
@@ -0,0 +1,24 @@
+from locust import HttpUser, task, between
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 3)          # pause between tasks
+
+    @task(3)
+    def load_homepage(self):
+        self.client.get("/pages/homepage.html", name="Homepage (static)")
+
+    @task(2)
+    def login_success(self):
+        self.client.post(
+            "/pages/login.html",
+            {"user": "admin", "pass": "secret"},
+            name="Login (success)",
+        )
+
+    @task(1)
+    def login_fail(self):
+        self.client.post(
+            "/pages/login.html",
+            {"user": "foo", "pass": "bar"},
+            name="Login (fail)",
+        )

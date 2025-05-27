e016a664c699fc21ae2f70b877533d39cdd7eea7
hunainsh-99 <hunainsheikh5@gmail.com>
2025-05-27 14:37:14 -0400

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,10 @@
 from selenium.webdriver.common.by import By
 
 class LoginPage:
-    URL = "{base_url}/pages/login.html"
-    USER = (By.ID, "user")
-    PASS = (By.ID, "pass")
-    SUBMIT = (By.CSS_SELECTOR, "#login-form button")
+    URL     = "{base_url}/pages/login.html"
+    USER    = (By.ID, "user")
+    PASS    = (By.ID, "pass")
+    SUBMIT  = (By.CSS_SELECTOR, "#login-form button")
     MESSAGE = (By.ID, "message")
 
     def __init__(self, driver, base_url):
@@ -15,13 +15,13 @@ class LoginPage:
         self.driver.get(self.URL.format(base_url=self.base_url))
 
     def submit(self, username, password):
-        elem_user = self.driver.find_element(*self.USER)
-        elem_user.clear()
-        elem_user.send_keys(username)
-        elem_pass = self.driver.find_element(*self.PASS)
-        elem_pass.clear()
-        elem_pass.send_keys(password)
-        self.driver.find_element(*self.SUBMIT).click()
+        el = self.driver.find_element
+        el(*self.USER).clear()
+        el(*self.USER).send_keys(username)
+        el(*self.PASS).clear()
+        el(*self.PASS).send_keys(password)
+        el(*self.SUBMIT).click()
 
     def message_text(self):
         return self.driver.find_element(*self.MESSAGE).text
+
